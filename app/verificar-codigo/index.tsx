920bbb616ab5f3682ab6777bee5e12b46abc45ca
Katiana Hanisch <katianahanisch@gmail.com>
2024-04-17 14:29:29 -0400

    feat: adicionar modal de resposta ao verificar email

--- app/verificar-codigo/index.tsx
+++ app/verificar-codigo/index.tsx
@@ -1,84 +1,112 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-import { Text, View, StyleSheet, TextInput } from "react-native";
+import { Text, View, StyleSheet, TextInput, Image } from "react-native";
 
 import { ImagemTelaLogin } from "@/assets/images/imagem-tela-login";
 import { Button } from "@/components/button";
 
 import { theme } from "@/constants";
+import { ModalStatusVerificarEmail } from "@/components/modal-status-verificar-email";
 
 export default function VerificarEmail() {
+  const [abrirModal, setAbrirModal] = useState<boolean>(false);
+
   const inputRef1 = useRef<TextInput | null>(null);
   const inputRef2 = useRef<TextInput | null>(null);
   const inputRef3 = useRef<TextInput | null>(null);
   const inputRef4 = useRef<TextInput | null>(null);
 
   return (
-    <View style={styles.container}>
-      <ImagemTelaLogin />
-      <View style={styles.containerItens}>
-        <View style={styles.containerTextos}>
-          <Text style={styles.textoTitulo}>
-            Digite seu código de verificação
-          </Text>
-          <Text style={styles.textoSubtitulo}>
-            Enviamos um código de verificação para seu email. Verifique sua
-            caixa de entrada
-          </Text>
+    <>
+      {abrirModal ? (
+        <>
+          <Image
+            style={styles.imagem}
+            source={require("../../assets/images/imagem-cadastro.png")}
+          />
+          <ModalStatusVerificarEmail
+            status="sucesso"
+            titulo="Cadastro criado com sucesso"
+            subTitulo="Seu cadastro foi concluído com sucesso. Realize seu login"
+            tituloButton="Realizar login"
+            rota="login"
+          />
+        </>
+      ) : (
+        <View style={styles.container}>
+          <ImagemTelaLogin />
+          <View style={styles.containerItens}>
+            <View style={styles.containerTextos}>
+              <Text style={styles.textoTitulo}>
+                Digite seu código de verificação
+              </Text>
+              <Text style={styles.textoSubtitulo}>
+                Enviamos um código de verificação para seu email. Verifique sua
+                caixa de entrada
+              </Text>
+            </View>
+            <View style={styles.containerInputs}>
+              <TextInput
+                ref={inputRef1}
+                style={styles.input}
+                keyboardType="numeric"
+                maxLength={1}
+                cursorColor={theme.colors.grayLight}
+                onChangeText={(text) => {
+                  if (text.length === 1) {
+                    inputRef2.current?.focus();
+                  }
+                }}
+              />
+              <TextInput
+                ref={inputRef2}
+                style={styles.input}
+                keyboardType="numeric"
+                maxLength={1}
+                cursorColor={"#6b6b6b"}
+                onChangeText={(text) => {
+                  if (text.length === 1) {
+                    inputRef3?.current?.focus();
+                  } else if (text.length === 0) {
+                    inputRef1.current?.focus();
+                  }
+                }}
+              />
+              <TextInput
+                ref={inputRef3}
+                style={styles.input}
+                keyboardType="numeric"
+                maxLength={1}
+                cursorColor={"#6b6b6b"}
+                onChangeText={(text) => {
+                  if (text.length === 1) {
+                    inputRef4?.current?.focus();
+                  } else if (text.length === 0) {
+                    inputRef2.current?.focus();
+                  }
+                }}
+              />
+              <TextInput
+                ref={inputRef4}
+                style={styles.input}
+                keyboardType="numeric"
+                maxLength={1}
+                cursorColor={"#6b6b6b"}
+                onChangeText={(text) => {
+                  if (text.length === 0) {
+                    inputRef3.current?.focus();
+                  }
+                }}
+              />
+            </View>
+            <Button
+              titulo="Verificar código"
+              handleNavigation={() => setAbrirModal(true)}
+            />
+          </View>
         </View>
-        <View style={styles.containerInputs}>
-          <TextInput
-            ref={inputRef1}
-            style={styles.input}
-            keyboardType="numeric"
-            maxLength={1}
-            onChangeText={(text) => {
-              if (text.length === 1) {
-                inputRef2.current?.focus();
-              }
-            }}
-          />
-          <TextInput
-            ref={inputRef2}
-            style={styles.input}
-            keyboardType="numeric"
-            maxLength={1}
-            onChangeText={(text) => {
-              if (text.length === 1) {
-                inputRef3?.current?.focus();
-              } else if (text.length === 0) {
-                inputRef1.current?.focus();
-              }
-            }}
-          />
-          <TextInput
-            ref={inputRef3}
-            style={styles.input}
-            keyboardType="numeric"
-            maxLength={1}
-            onChangeText={(text) => {
-              if (text.length === 1) {
-                inputRef4?.current?.focus();
-              } else if (text.length === 0) {
-                inputRef2.current?.focus();
-              }
-            }}
-          />
-          <TextInput
-            ref={inputRef4}
-            style={styles.input}
-            keyboardType="numeric"
-            maxLength={1}
-            onChangeText={(text) => {
-              if (text.length === 0) {
-                inputRef3.current?.focus();
-              }
-            }}
-          />
-        </View>
-        <Button titulo="Verificar código" />
-      </View>
-    </View>
+      )}
+    </>
   );
 }
 
@@ -129,7 +157,13 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.grayLight,
     borderRadius: 10,
     textAlign: "center",
-    fontSize: 35,
+    fontSize: 32,
     color: theme.colors.grayDark,
+  },
+  imagem: {
+    position: "absolute",
+    top: 0,
+    zIndex: -99,
+    marginBottom: 100,
   },
 });
--- components/modal-status-verificar-email.tsx
+++ components/modal-status-verificar-email.tsx
@@ -9,10 +9,26 @@ import { Button } from "./button";
 import { theme } from "@/constants";
 
 import { ImagemVerificarEmail } from "@/assets/images/ImageVerificarEmail";
+import { IconDone } from "@/assets/icons/icon-done";
+import { IconError } from "@/assets/icons/icon-error";
 
-export function ModalVerificarEmail() {
+interface ModalStatusVerificarEmailProps {
+  status: string;
+  titulo: string;
+  subTitulo: string;
+  tituloButton: string;
+  rota?: string;
+}
+
+export function ModalStatusVerificarEmail({
+  status,
+  titulo,
+  subTitulo,
+  tituloButton,
+  rota,
+}: ModalStatusVerificarEmailProps) {
   function handleNavigation() {
-    router.navigate("verificar-codigo");
+    router.navigate(rota || "login");
   }
 
   return (
@@ -20,17 +36,12 @@ export function ModalVerificarEmail() {
       <StatusBar style="light" />
       <View style={styles.container}>
         <View style={styles.containerModal}>
-          <ImagemVerificarEmail />
+          {status === "sucesso" ? <IconDone /> : <IconError />}
           <View style={styles.containerTextos}>
-            <Text style={styles.textoTitulo}>
-              Confirmar seu endereço de email
-            </Text>
-            <Text style={styles.textoSubtitulo}>
-              Seu cadastro está quase pronto. Mas precisamos que confirme seu
-              endereço de email.
-            </Text>
+            <Text style={styles.textoTitulo}>{titulo}</Text>
+            <Text style={styles.textoSubtitulo}>{subTitulo}</Text>
           </View>
-          <Button titulo="Enviar email" handleNavigation={handleNavigation} />
+          <Button titulo={tituloButton} handleNavigation={handleNavigation} />
         </View>
       </View>
     </>
@@ -46,16 +57,18 @@ const styles = StyleSheet.create({
   },
   containerModal: {
     width: "95%",
-    height: "65%",
+    height: "60%",
     backgroundColor: theme.colors.white,
     borderRadius: 30,
     alignItems: "center",
-    padding: 25,
+    justifyContent: "center",
+    padding: 20,
     gap: 20,
   },
   containerTextos: {
     width: "100%",
     gap: 5,
+    marginBottom: 10,
   },
   textoTitulo: {
     fontSize: 22,
@@ -63,7 +76,6 @@ const styles = StyleSheet.create({
     color: theme.colors.grayMedium,
     width: "100%",
     textAlign: "left",
-    lineHeight: 22,
   },
   textoSubtitulo: {
     fontSize: 16,
